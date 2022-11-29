361e7bec87bd502a16b7f258f49cd4ce202a9aea
anushagh <hanagandianusha@gmail.com>
2022-11-29 18:32:44 +0530

    Fine tune the text processing functionality to match the expected output

--- MyApp/src/main/kotlin/MyApp/ContentController.kt
+++ MyApp/src/main/kotlin/MyApp/ContentController.kt
@@ -10,7 +10,28 @@ import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Produces
 import io.micronaut.json.tree.JsonObject
 
-
+fun textProcess(text:String): MutableMap<String, Int>{
+    var text = text.lowercase()
+    val words= mutableListOf<String>()
+    words.addAll(text.split(" "))
+    val wordCount = words.size
+    val charCountWithSpaces = text.length
+    val newText = text.replace(" ", "").replace("\n", "").replace("\r", "").replace(".", "").replace(",","")
+    val charCountWoSpaces = newText.length
+    val lineCount = text.filter {it == '.'}.count()
+    words.clear()
+    text = text.replace(".", " ").replace("'s", "").replace(",", "")
+    words.addAll(text.split(" "))
+    val uniqSet = words.toSet()
+    val uniq = uniqSet.size
+    return mutableMapOf(
+        "word_count" to wordCount,
+        "character_count_with_spaces" to charCountWithSpaces,
+        "character_count_without_spaces" to charCountWoSpaces,
+        "line_count" to lineCount,
+        "unique_words" to uniq
+    )
+}
 @Controller("/text/analyze")
 class ContentController {
 
@@ -18,23 +39,7 @@ class ContentController {
     @Post //
     fun textAnalysis(@Body content :JsonObject): HttpResponse<*> {
         var text = content.get("content").stringValue
-        text = text.lowercase()
-        val words: List<String> = text.split(" ")
-        val wordCount = words.size
-        val charCountWithSpaces = text.length
-        val newText = text.replace(" ", "").replace("\n", "").replace("\r", "")
-        val charCountWoSpaces = newText.length
-        val lineCount = text.filter {it == '.'}.count()
-        val uniqSet = words.toSet()
-        val uniq = uniqSet.size
-        val result = mutableMapOf(
-            "word_count" to wordCount,
-            "character_count_with_spaces" to charCountWithSpaces,
-            "character_count_without_spaces" to charCountWoSpaces,
-            "line_count" to lineCount,
-            "unique_words" to uniq
-        )
-
+        val result = textProcess(text)
         return HttpResponse.ok(result)
 
     }
